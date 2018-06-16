825896881699c88d506ffaee4b6ab3a0491d082e
Michael Bowman <bowmanmc@gmail.com>
2018-06-15 21:35:53 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from './components/header';
+
+import './App.css';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css';
+
+
+const Header = props => {
+    return (
+        <div className="Header">
+            <h1>Howdy</h1>
+        </div>
+    );
+};
+
+export default Header;
