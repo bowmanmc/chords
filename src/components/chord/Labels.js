462d2d132e9b48163c84b7a692d840b05c790676
Michael Bowman <bowmanmc@gmail.com>
2018-06-23 09:50:13 -0400

    animate in dots

--- src/components/chord/Labels.js
+++ src/components/chord/Labels.js
@@ -16,7 +16,7 @@ const Labels = props => {
                     if (firstFret === 0) {
                         y += config.nutSize;
                     }
-                    return <text key={i} alignmentBaseline='middle' x={3} y={y}>{fretNum}</text>;
+                    return <text key={i} x={3} y={y+1.5}>{fretNum}</text>;
                 })}
             </g>
         );
