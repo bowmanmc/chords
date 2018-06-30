f15c46922fbac87a484bd78a1cd8e8b32f345285
Michael Bowman <bowmanmc@gmail.com>
2018-06-29 20:42:45 -0400

    logo

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends React.Component {
         return (
             <div className="App">
                 <Header selected={root} onRootChange={(root) => {
-                    console.log('Updating to root: ' + root);
                     this.setState({root});
                 }}/>
                 <div className="Content">
