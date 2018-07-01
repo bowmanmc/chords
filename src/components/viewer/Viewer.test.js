a1807c3c152bdec9c094dec4bd54d9fba1b5e763
Michael Bowman <bowmanmc@gmail.com>
2018-07-01 19:46:22 -0400

    tests

--- src/components/viewer/Viewer.test.js
+++ src/components/viewer/Viewer.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import Viewer from './index';
+
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    ReactDOM.render(<Viewer root={'c'} />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
