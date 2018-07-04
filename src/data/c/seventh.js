db50e5dc04aaf49c0f1b2e1ae98b9f14934ee57e
Michael Bowman <bowmanmc@gmail.com>
2018-07-04 15:20:57 -0400

    C7

--- src/data/c/seventh.js
+++ src/data/c/seventh.js
@@ -1,24 +1,17 @@
+import MovableSeventh from "../movable/seventh";
+
+
 export default {
     name: 'C Seventh',
     shortName: 'C7',
     root: 'C',
     quality: '7',
-    positions: [{
-        tabs:      ['x','1','2','0','1','0'],
-        fingering: ['x','1','3','0','2','0']
-    }, {
-        tabs:      ['x','3','2','3','1','0'],
-        fingering: ['x','3','2','4','1','0']
-    }, {
-        tabs:      ['x','3','5','3','5','3'],
-        fingering: ['x','1','3','1','4','1']
-    }, {
-        startFret: 7,
-        tabs:      ['8','x','8','9','8','x'],
-        fingering: ['1','x','2','4','3','x']
-    }, {
-        startFret: 9,
-        tabs:      ['x','x','10','12','11','12'],
-        fingering: ['x','x','1','2','2','4']
-    }]
+    positions: [
+        MovableSeventh.A3(3),
+        MovableSeventh.A1(3),
+        MovableSeventh.A2(3),
+        MovableSeventh.E1(8),
+        MovableSeventh.E2(8),
+        MovableSeventh.D1(10)
+    ]
 };
