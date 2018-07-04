3d2071d87b8e5e910de54d845e7e40aa09660bc6
Michael Bowman <bowmanmc@gmail.com>
2018-07-04 15:24:24 -0400

    C#7

---
+++ src/data/csharp/seventh.js
@@ -0,0 +1,17 @@
+import MovableSeventh from '../movable/seventh';
+
+
+export default {
+    name: 'C♯ / D♭ Seventh',
+    shortName: 'C♯7 / D♭7',
+    root: 'C♯ / D♭',
+    quality: 'seventh',
+    positions: [
+        MovableSeventh.A1(4),
+        MovableSeventh.A2(4),
+        MovableSeventh.A3(4),
+        MovableSeventh.E1(9),
+        MovableSeventh.E2(9),
+        MovableSeventh.D1(11)
+    ]
+};
