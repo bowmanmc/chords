231cddd27e63142c6b3bf5f00a26465b3129ca13
Michael Bowman <bowmanmc@gmail.com>
2018-07-04 15:59:53 -0400

    D#7

---
+++ src/data/dsharp/seventh.js
@@ -0,0 +1,17 @@
+import MovableSeventh from '../movable/seventh';
+
+
+export default {
+    name: 'D♯ / E♭ Seventh',
+    shortName: 'D♯7 / E♭7',
+    root: 'D♯ / E♭',
+    quality: 'seventh',
+    positions: [
+        MovableSeventh.D1(1),
+        MovableSeventh.A1(6),
+        MovableSeventh.A2(6),
+        MovableSeventh.A3(6),
+        MovableSeventh.E1(11),
+        MovableSeventh.E2(11)
+    ]
+};
