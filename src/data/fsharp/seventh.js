613cabedbecb9c74fbbe3414c7343753499f6a9c
Michael Bowman <bowmanmc@gmail.com>
2018-07-04 18:45:06 -0400

    F#7

---
+++ src/data/fsharp/seventh.js
@@ -0,0 +1,17 @@
+import MovableSeventh from '../movable/seventh';
+
+
+export default {
+    name: 'F♯ / G♭ Seventh',
+    shortName: 'F♯7 / G♭7',
+    root: 'F♯ / G♭',
+    quality: 'seventh',
+    positions: [
+        MovableSeventh.E1(2),
+        MovableSeventh.E2(2),
+        MovableSeventh.D1(4),
+        MovableSeventh.A1(9),
+        MovableSeventh.A2(9),
+        MovableSeventh.A3(9)
+    ]
+};
