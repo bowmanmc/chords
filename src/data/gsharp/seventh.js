011c9936b402d03ad1590edcc83c4615e5fb268a
Michael Bowman <bowmanmc@gmail.com>
2018-07-04 19:24:09 -0400

    G#7

---
+++ src/data/gsharp/seventh.js
@@ -0,0 +1,17 @@
+import MovableSeventh from '../movable/seventh';
+
+
+export default {
+    name: 'G♯ / A♭ Seventh',
+    shortName: 'G♯7 / A♭7',
+    root: 'G♯ / A♭',
+    quality: 'seventh',
+    positions: [
+        MovableSeventh.E1(4),
+        MovableSeventh.E2(4),
+        MovableSeventh.D1(6),
+        MovableSeventh.A1(11),
+        MovableSeventh.A2(11),
+        MovableSeventh.A3(11)
+    ]
+};
