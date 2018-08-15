e8dabe274db826b5220588d0fb5dc263c6894581
Michael Bowman <bowmanmc@gmail.com>
2018-08-15 17:16:37 -0400

    Icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,14 +9,32 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/:chord" component={App} />
-            <Route path="/:chord/:quality" component={App} />
-        </Switch>
-    </BrowserRouter>,
-    document.getElementById('root')
-);
-registerServiceWorker();
+const startApplication = () => {
+    ReactDOM.render(
+        <HashRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/:chord" component={App} />
+                <Route path="/:chord/:quality" component={App} />
+            </Switch>
+        </HashRouter>,
+        document.getElementById('root')
+    );
+    registerServiceWorker();
+};
+
+/*
+ * Check to see if we are running as an app or in a web browser. If we're
+ * in an app, we need to wait for the "deviceready" event to fire before
+ * we start things up.
+ */
+if(!window.cordova) {
+    // We're running in a web browser. Since we have the script tag at the
+    // end of the file, we can just call startApplication
+    startApplication();
+}
+else {
+    // We're in cordova. Register an event listener for the deviceready
+    // event for when we can start rendering our application to the DOM
+    document.addEventListener('deviceready', startApplication, false);
+}
